da794dbf4b71add1cf468530d03317b00ba03797
Αβραάμ Λιαούτσης <av.liaoutsis@outlook.com>
2020-04-26 13:30:55 +0300

    Initial Commit

---
+++ TinyCRMConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyCRMConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
